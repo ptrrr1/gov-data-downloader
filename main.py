08c1c1d5187e0a0d11e336786fa0bdbd0483ebf7
ptrrr1 <limahenriquec@gmail.com>
2024-09-17 17:31:37 -0300

    configuring sql connection

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 from downloader import Downloader
+from sqlhandler import SQLHandler
+import pandas as pd
+from sqlalchemy import create_engine
 
 # Extract to
 output = "./output"
@@ -8,8 +12,119 @@ url = "https://dadosabertos.rfb.gov.br/CNPJ/dados_abertos_cnpj/"
 whitelist = []
 
 # Donwloading only the most recent month
-Downloader(url, output, whitelist).async_recursive_download(start_range=-1)
+# Downloader(url, output, whitelist).async_recursive_download(start_range=-1)
 
 # WARN: Throwing a 404 on async download despite having a status of 200
 # url = "https://dados.rfb.gov.br/CNPJ/"
 # Downloader(url, output_path).recursive_download(start_range=1)
+
+extracted_files = [os.path.join(output, d) for d in os.listdir(output)]
+extracted_files = [d for d in extracted_files if os.path.isdir(d)]
+sorted(extracted_files, key=lambda x: os.path.getctime(x), reverse=True)[:1]
+
+# List files
+items = [n for n in os.listdir(extracted_files[0]) if not n.endswith('.zip')]
+
+# Grouping files
+simples = []  # 1 File - 2GB
+cnae = []  # 1 File
+moti = []  # 1 File
+munic = []  # 1 File
+natju = []  # 1 File
+pais = []  # 1 File
+quals = []  # 1 File
+empre = []  # 10 Files - 4GB Total
+estabele = []  # 10 Files - 13GB Total
+socio = []  # 10 Files - 2GB Total
+
+for i in items:
+    if i.find("SIMPLES") > -1:
+        simples.append(i)
+    elif i.find("CNAE") > -1:
+        cnae.append(i)
+    elif i.find("MOTI") > -1:
+        moti.append(i)
+    elif i.find("MUNIC") > -1:
+        munic.append(i)
+    elif i.find("NATJU") > -1:
+        natju.append(i)
+    elif i.find("PAIS") > -1:
+        pais.append(i)
+    elif i.find("QUALS") > -1:
+        quals.append(i)
+    elif i.find("EMPRE") > -1:
+        empre.append(i)
+    elif i.find("ESTABELE") > -1:
+        estabele.append(i)
+    elif i.find("SOCIO") > -1:
+        socio.append(i)
+    else:
+        print(i)
+
+# Smaller Files
+
+cnae_dtype = {'codigo': 'object', 'descricao': 'object'}
+
+cnae_pd = SQLHandler(
+    extracted_files,
+    cnae,
+    cnae_dtype
+    ).to_sql('cnae', engine)
+
+# moti_dtype = {'codigo': 'object', 'descricao': 'object'}
+# 
+# moti_pd = SQLHandler(
+#     extracted_files,
+#     moti,
+#     moti_dtype
+#     ).to_sql('moti'), engine
+# 
+# munic_dtype = {'codigo': 'object', 'descricao': 'object'}
+# 
+# munic_pd = SQLHandler(
+#     extracted_files,
+#     munic,
+#     munic_dtype
+#     ).to_sql('munic', engine)
+# 
+# natju_dtype = {'codigo': 'object', 'descricao': 'object'}
+# 
+# natju_pd = SQLHandler(
+#     extracted_files,
+#     natju,
+#     natju_dtype
+#     ).to_sql('natju', engine)
+# 
+# pais_dtype = {'codigo': 'object', 'descricao': 'object'}
+# 
+# pais_pd = SQLHandler(
+#     extracted_files,
+#     pais,
+#     pais_dtype
+#     ).to_sql('pais'), engine
+# 
+# quals_dtype = {'codigo': 'object', 'descricao': 'object'}
+# 
+# quals_pd = SQLHandler(
+#     extracted_files,
+#     quals,
+#     quals_dtype
+#     ).to_sql('quals', engine)
+
+# Bigger Files
+
+# simples_dtype = {
+#         'cnpj_basico': object,
+#         'opcao_pelo_simples': object,
+#         'data_opcao_simples': 'Int32',
+#         'data_exclusao_simples': 'Int32',
+#         'opcao_mei': object,
+#         'data_opcao_mei': 'Int32',
+#         'data_exclusao_mei': 'Int32'
+#         }
+# 
+# simples_pd = SQLHandler(
+#     extracted_files,
+#     simples,
+#     simples_dtype
+#     ).to_sql()
--- sqlhandler.py
+++ sqlhandler.py
@@ -1,3 +1,80 @@
+import os
+import math
+import pandas as pd
+
+
 class SQLHandler:
-    def __init__(self):
-        pass
+    def __init__(self, folder, path_list, dtypes):
+        self.path_list = path_list
+        self.folder = folder
+        self.dtypes = dtypes
+
+    def as_pdDataframe(self) -> pd.DataFrame:
+        dt = pd.DataFrame(columns=self.dtypes.keys()).astype(self.dtypes)
+        size_to_read = 1_000_000
+        lines = 0
+        skiprows = 0
+
+        for f in self.path_list:
+            path = os.path.join(self.folder, f)
+
+            with open(path) as file:
+                lines = sum(1 for _ in file)
+
+            chunk = math.ceil(lines / size_to_read)
+
+            for i in range(0, chunk):
+                df = pd.read_csv(
+                    filepath_or_buffer=path,
+                    sep=';',
+                    nrows=size_to_read,
+                    skiprows=skiprows,
+                    header=None,
+                    dtype=self.dtypes,
+                    names=self.dtypes.keys(),
+                    encoding='latin-1',
+                    )
+
+                df.reset_index()
+                dt = pd.concat([dt, df], ignore_index=True)
+
+                skiprows += size_to_read
+
+        return dt
+
+    def to_sql(self, name, engine):
+        size_to_read = 1_000_000
+        lines = 0
+        skiprows = 0
+
+        for f in self.path_list:
+            path = os.path.join(self.folder, f)
+
+            with open(path) as file:
+                lines = sum(1 for _ in file)
+
+            chunk = math.ceil(lines / size_to_read)
+
+            for i in range(0, chunk):
+                df = pd.read_csv(
+                    filepath_or_buffer=path,
+                    sep=';',
+                    nrows=size_to_read,
+                    skiprows=skiprows,
+                    header=None,
+                    dtype=self.dtypes,
+                    names=self.dtypes.keys(),
+                    encoding='latin-1',
+                    )
+
+                df.reset_index()
+
+                skiprows += size_to_read
+
+                df.to_sql(
+                    df,
+                    name=name,
+                    con=engine,
+                    if_exists='append',
+                    index=False
+                    )
